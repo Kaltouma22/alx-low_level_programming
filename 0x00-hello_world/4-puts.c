5aae13f71198ca188f45a07ac925991e73b820f9
kaltoumchb <kaltoum@gmail.com>
2023-09-07 16:15:01 +0100

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: had lbarnamaj C kytba3 ay haja bghitiha b put
+ *
+ * Return: Alwayz 0 (Success
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
