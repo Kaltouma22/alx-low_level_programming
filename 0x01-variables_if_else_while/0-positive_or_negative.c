5a93aa0488c566c45571afd2aea7eea1bb5dcf65
kaltoumchb <kaltoum@gmail.com>
2023-09-08 15:20:45 +0100

    positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a positive or negative or zero. 
+ *
+ * Return: Alwayz 0 (success)
+*/
+
 int main(void)
 {
 	int n;
