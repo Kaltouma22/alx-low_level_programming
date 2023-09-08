31fb32e2acca90cab3e902c7abb5497792791907
kaltoumchb <kaltoum@gmail.com>
2023-09-08 17:11:58 +0100

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet lowercase and uppercase.
+ *
+ * Return: Alwayz 0 (success)
+*/
+
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; alphabet[i] != '\0'; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}		
