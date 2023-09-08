e9a0e1abfc6d451ce492f437172f3c732dd4674e
kaltoumchb <kaltoum@gmail.com>
2023-09-08 19:18:03 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints all alphabet.
+ *
+ * Return: Alwayz 0 (success)
+*/
+
+int main(void)
+{
+	int (i);
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 10; i <= 15; i++)
+	{
+		putchar('a' + i - 10);
+	}
+	putchar('\n');
+	return 0;
+}
