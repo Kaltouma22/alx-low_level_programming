19352595ff3e9fde4431431dae5f07a38784c430
kaltoumchb <kaltoum@gmail.com>
2023-09-12 20:31:53 +0100

    minute and hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - checks for print every minut of the day.
+*/
+
+void jack_bauer(void)
+{
+	int h, m;
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf("%02d:%02d\n", h, m);
+		}
+	}
+}
