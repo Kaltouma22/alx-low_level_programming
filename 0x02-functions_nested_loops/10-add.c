0c553618a68df12569d41115f5e1b20114569558
kaltoumchb <kaltoum@gmail.com>
2023-09-12 22:25:44 +0100

    add number to number

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,11 @@
 
 /**
  * add - function that adds two integers and returns the result.
- */
+ * @a: use for addition
+ * @b: use for addition
+ * Return: return (a + b)
+*/
+
 int add(int a, int b)
 {
 	return (a + b);
