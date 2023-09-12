0846e1922fa3c592bb2e63ae5a1e2f95d4ff8d53
kaltoumchb <kaltoum@gmail.com>
2023-09-12 17:55:40 +0100

    print lowercase and uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for uppercase character.
+ *
+ * @c: use for check result of function
+ *
+ * Return: return 1 for `c` is lowercase or uppercase
+ * otherwise always 0
+*/
+
+int _isalpha(int c)
+{
+        if (c > 96 && c < 123)
+		return (1);
+	
+	if (c > 64 && c < 91)
+                return (1);
+	
+	return (0);
+}
+
