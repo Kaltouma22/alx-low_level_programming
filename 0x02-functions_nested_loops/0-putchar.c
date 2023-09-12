f4aa7eb257a916ad1aea313e83055555eb7e05d9
kaltoumchb <kaltoum@gmail.com>
2023-09-12 12:27:33 +0100

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *i Description: A C program that prints a word.
+ *
+ * Return: Alwayz 0 (success)
+*/
+
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
