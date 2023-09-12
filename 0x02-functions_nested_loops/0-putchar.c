903f4d50010203a93cd14f868fa858d2e25b5822
kaltoumchb <kaltoum@gmail.com>
2023-09-12 12:49:58 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *i Description: A C program that prints all possible combination of numbers.
+ *
+ * Return: Alwayz 0 (success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
