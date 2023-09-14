e3a710b1afa6b4407cf810d56618bf33233e1be2
kaltoumchb <kaltoum@gmail.com>
2023-09-14 18:31:31 +0100

    s

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - that draws a straight line
+ * @n: for check result
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
