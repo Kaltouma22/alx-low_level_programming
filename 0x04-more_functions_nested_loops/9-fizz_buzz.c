14629c000a8c68eaebd0c62c9c5dba46a5dca7a6
kaltoumchb <kaltoum@gmail.com>
2023-09-14 21:37:00 +0100

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *i Description: A C program that prints Fizz-Buzz test.
+ *
+ * Return: Alwayz 0
+*/
+
+int main(void)
+{
+	int (i);
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 != 0) && (i % 5 != 0))
+			printf("%d", i);
+		if (i % 3 == 0)
+			printf("fizz");
+		if (i % 5 == 0)
+			printf("buzz");
+		printf(" ");
+	}
+	putchar('\n');
+	return (0);
+}
