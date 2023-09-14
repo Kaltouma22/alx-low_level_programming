120d43a01d8c4c9711439bfb16f922ba79bc9bbd
kaltoumchb <kaltoum@gmail.com>
2023-09-14 15:27:43 +0100

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _mul - checks for calcul the multiplication.
+ *
+ * @a: first input in function
+ * @b: second input in function
+ *
+ * Return: return a * b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
