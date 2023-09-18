054a4eef43b89599fe0d960cacce6d5028bbe781
kaltoumchb <kaltoum@gmail.com>
2023-09-18 22:52:23 +0100

    d

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 -  that prints every other character of a string
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+	for (i = 0; i < len; i=i+2)
+	{
+		putchar(str[i]);;
+	}
+	putchar('\n');
+}
