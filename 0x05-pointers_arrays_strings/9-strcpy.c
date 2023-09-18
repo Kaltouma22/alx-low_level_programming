33ac11a0ad05237a0118ac17072ef39e1888d44a
kaltoumchb <kaltoum@gmail.com>
2023-09-19 00:12:30 +0100

    push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy - that copies the string pointed
+ * @dest: this is for output
+ * @src: for check resulta
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do
+	{
+		i++;
+		dest [i] = src[i];
+	}
+	while (src[i] != '\0');
+	return (dest);
+}
