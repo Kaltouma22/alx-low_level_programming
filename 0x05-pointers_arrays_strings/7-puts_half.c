7f7de9c522df76a9c4fd95388671210cf0499e00
kaltoumchb <kaltoum@gmail.com>
2023-09-18 23:24:01 +0100

    s

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,12 @@ void puts_half(char *str)
 		len++;
 	if (len % 2 != 0)
 	{
-		for (i = (len - 1) / 2; i < len; i++)
+		for (i = (len / 2) + 1; i < len; i++)
 		{
 			putchar(str[i]);
 		}
 	}
-	else 
+	else
 	{
 		for (i = len / 2; i < len; i++)
 		{
