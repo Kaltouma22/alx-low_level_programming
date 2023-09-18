e3619ce73812f35774f3d2c6e54fc1bc2c8eebda
kaltoumchb <kaltoum@gmail.com>
2023-09-19 00:28:42 +0100

    s

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,7 +7,7 @@
  * Description: this function for copies the string pointed to by src
  * @dest: this is for output
  * @src: for check resulta
- * Return : return dest
+ * Return : return to point dest
 */
 
 char *_strcpy(char *dest, char *src)
@@ -18,6 +18,6 @@ char *_strcpy(char *dest, char *src)
 		i++;
 		dest[i] = src[i];
 	} while (src[i] != '\0');
-	
+
 	return (dest);
 }
