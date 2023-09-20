81aca02ca8155a22f54b73381ef7e01a428785bf
kaltoumchb <kaltoum@gmail.com>
2023-09-20 23:00:45 +0100

    d

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	bool new_word = true;
+
+	while (*ptr != '\0')
+	{
+		if (new_word && (*ptr >= 'a' && *ptr <= 'z'))
+		{
+			*ptr = *ptr - ('a' - 'A');
+			new_word = false;
+		}
+		else if (*ptr == ' ' || *ptr == '\t' || *ptr == '\n' || *ptr == ',' || *ptr == ';' || *ptr == '.' || *ptr == '!' || *ptr == '?' || *ptr == '"' || *ptr == '(' || *ptr == ')' || *ptr == '{' || *ptr == '}')
+		{
+			new_word = true;
+		}
+		else
+		{
+			new_word = false;
+		}
+		ptr++;
+	}
+	return (str);
+}
+
