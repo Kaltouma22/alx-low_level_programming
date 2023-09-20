7b0a10afb01943f5fb043f1bf5b2e7cf9252865d
kaltoumchb <kaltoum@gmail.com>
2023-09-20 18:47:04 +0100

    cpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strncpy - that copies a string
+ *
+ * @dest: this is the first string
+ * @src: second string
+ * @n: for bytes
+ *
+ * Return: return (dest)
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n && src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+	if (a < n)
+	{
+		dest[a] = '\0';
+		a++;
+	}
+
+	return (dest);
+}
