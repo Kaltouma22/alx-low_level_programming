467004c579738d5cf538dfa56b1ecd83da32400c
kaltoumchb <kaltoum@gmail.com>
2023-09-28 19:34:49 +0100

    d

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,21 +4,39 @@
 /**
  * is_palindrome - that returns 1 if a string is a palindrome and 0 if not.
  * @s: The string to be printed
+ * @i: for check if the number is palindrome
+ * @e: this is also for check palindrome number
  *
  * Return: return 1 if a string is a palindrome and 0 if not
 */
 
+int isPalRec(char *s, int i, int e);
 int is_palindrome(char *s)
 {
-	int len = strlen(s);
-	int i, j;
+	int n = strlen(s);
 
-	for (i = 0, j = len - 1; i < j; i++, j--)
-	{
-		if (s[i] != s[j])
-		{
-			return (0);
-		}
-	}
+	if (n == 0)
+		return (1);
+
+	return (isPalRec(s, 0, n - 1));
+}
+
+/**
+ * isPalRec - that for recursion
+ * @s: the string to be printed
+ * @i: for check if the number is palindrome
+ * @e: this is also for check palindrome number
+ *
+ * Return: return isPalRec
+*/
+
+int isPalRec(char *s, int i, int e)
+{
+	if (i == e)
+		return (1);
+	if (s[i] != s[e])
+		return (0);
+	if (i < e + 1)
+		return (isPalRec(s, i + 1, e - 1));
 	return (1);
 }
