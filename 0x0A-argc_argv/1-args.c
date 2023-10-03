5991abd0910454d366fe73bab16e7b52a8abf19b
kaltoumchb <kaltoum@gmail.com>
2023-10-03 11:31:23 +0100

    d

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-/***/
+/**
+ * main - that print number of arguments
+ * @argc: represents the number of arguments
+ * @argv: is an array of strings that stores these argument
+ * Return : return (0)
+*/
 
 int main(int argc, char *argv[])
 {
