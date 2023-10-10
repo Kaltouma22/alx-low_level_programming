27a798a7b429e41acf817492984b8a488e04fb20
kaltoumchb <kaltoum@gmail.com>
2023-10-10 20:07:25 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct dog
+ * @name: the dog name
+ * @age: dog age
+ * @owner: owner name
+ *
+ * Description: just a lone dog struct in a big world
+ */
+
+struct dog
+{
+	char *name;
+	char age;
+	char *owner;
+};
+
+#endif
