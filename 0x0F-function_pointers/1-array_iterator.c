b7e5d7f829166c46436aa943d0ac7e4144bbfe39
kaltoumchb <kaltoum@gmail.com>
2023-10-11 18:50:59 +0100

    f

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator-that executes a function given as a parameter
+ * on each element of an array
+ * @array: A pointer to an array of integers
+ * @size: the size of the array
+ * @action: A function pointer representing the
+ * action to be executed on each element
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
 #ifndef _FUNCTION_POINTERS_H
 #define _FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 typedef int (*BinaryFunction)(int, int);
 
 int applyBinaryFunction(int a, int b, BinaryFunction function);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
