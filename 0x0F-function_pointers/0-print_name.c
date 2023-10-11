441fe1510fce9300abb2524859a983af17995519
kaltoumchb <kaltoum@gmail.com>
2023-10-11 17:53:12 +0100

    d

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,9 +5,11 @@
  * @name: pointer to a character array representing a name
  * @f: is a function pointer that specifies how the name
  * should be printed
+ * Return: return void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 	f(name);
 }
