14cde5b8e4f5eff9e843be942dceb9b4d6c100a5
kaltouma <kchbiki363@gmail.com>
2023-10-19 16:59:07 +0100

    first c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * my_constructor-Define a function and mark
+ * it with the constructor attribute.
+ *
+ * Return: return void.
+ */
+
+void my_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
