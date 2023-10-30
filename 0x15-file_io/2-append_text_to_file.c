e8350e462a7527ebf954ebdcd263155dac0e769c
kaltouma <kchbiki363@gmail.com>
2023-10-30 20:24:25 +0100

    ppenf

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename:
+ * @text_content:
+ *
+ * Retuen:
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		int bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
