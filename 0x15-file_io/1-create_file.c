84517e56d7623b1899498ce71c7b498dcc900a8a
kaltouma <kchbiki363@gmail.com>
2023-10-31 14:56:23 +0100

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,6 @@
 #include "main.h"
 
 /**
- * create_file - A function that creates a file
- * @filename: the name of the file to creat.
- * @text_content: the text content to write to the file
- *
- * Return: 1 on success, -1 on failure.
- * - Return -1 if file cannot be created, written, or if write failes
- * - Retuen 1 if the dile is created and written successfully
- */
-
-int create_file(const char *filename, char *text_content)
-{
-	int fd = open(filename, O_CREAT | O_WRONLY, 0600);
-
-	if (filename == NULL)
-		return (-1);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		int bytes_written = write(fd, text_content, strlen(text_content));
-
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (1);
-}
-
-/**
  * my_strlen - Calculate the length of a string.
  * @s: A pointer to the string to be measured.
  *
@@ -48,4 +16,48 @@ size_t my_strlen(const char *s)
 	while (*s++)
 		i++;
 	return (i);
+}
+
+/**
+ * create_file - A function that creates a file
+ * @filename: the name of the file to creat.
+ * @text_content: the text content to write to the file
+ *
+ * Return: 1 on success, -1 on failure.
+ * - Return -1 if file cannot be created, written, or if write failes
+ * - Retuen 1 if the dile is created and written successfully
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t bytes_written = 0;
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	if (close(fd) == -1)
+	{
+		return (-1);
+	}
+
+	return ((size_t)bytes_written == strlen(text_content) ? 1 : -1);
 }
