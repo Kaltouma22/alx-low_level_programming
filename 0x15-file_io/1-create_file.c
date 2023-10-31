657f3c4ab44102bbf859e54c4aeebda522b0b0ae
kaltouma <kchbiki363@gmail.com>
2023-10-31 15:11:25 +0100

    g

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,10 +4,10 @@
  * my_strlen - Calculate the length of a string.
  * @s: A pointer to the string to be measured.
  *
- * Return: The length of the string, or 0 if the input is NULL.
+ * Return: The int length of the string.
  */
 
-size_t my_strlen(const char *s)
+int my_strlen(char *s)
 {
 	int i = 0;
 
@@ -23,17 +23,15 @@ size_t my_strlen(const char *s)
  * @filename: the name of the file to creat.
  * @text_content: the text content to write to the file
  *
- * Return: 1 on success, -1 on failure.
- * - Return -1 if file cannot be created, written, or if write failes
- * - Retuen 1 if the dile is created and written successfully
+ * Return: 1 on success, 0 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t bytes_written = 0;
 	int fd;
+	ssize_t bytes = 0, slen = strlen(text_content);
 
-	if (filename == NULL)
+	if (!filename)
 	{
 		return (-1);
 	}
@@ -43,21 +41,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-
-	if (text_content != NULL)
+	if (slen)
 	{
-		bytes_written = write(fd, text_content, strlen(text_content));
-
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		bytes = write(fd, text_content, slen);
 	}
-	if (close(fd) == -1)
-	{
-		return (-1);
-	}
-
-	return ((size_t)bytes_written == strlen(text_content) ? 1 : -1);
+	close(fd);
+	return (bytes == slen ? 1 : -1);
 }
