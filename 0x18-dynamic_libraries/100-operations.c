2d7f32c47e4b521291fc450e0070ff4c170c874e
kaltouma <kchbiki363@gmail.com>
2023-12-11 19:06:38 +0100

    operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/* Addition function */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/* Subtraction function */
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+/* Multiplication function */
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/* Division function */
+int divide(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+	return (a / b);
+}
+
+/* Modulus function */
+int mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error: Modulus by zero");
+		return (0);
+	}
+	return (a % b);
+}
