c7d3daeb042a4a17698de86d8d197b13576a4451
Kaltouma22 <kchbiki363@gmail.com>
2023-12-22 17:21:06 +0100

    key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,5 +12,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value = hash_djb2(key);
 
-	return hash_value % size;
+	return (hash_value % size);
 }
